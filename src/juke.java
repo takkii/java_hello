a47e847e168dfef516ff0c0b5b8609872ed8acf5
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-04-29 19:33:28 +0900

    Update, コンストラクタ使用.

--- src/juke.java
+++ src/juke.java
@@ -3,16 +3,26 @@ import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
-public class juke{
-	public static void main(String[] args){
-		// TimeZone
-		LocalDateTime localDateTime = LocalDateTime.now();
-		ZonedDateTime dateTime = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
-		Date date = new Date();
+class juke{
+  public static void main(String[] args){
+	jruby jr = new jruby();
 
-		// Output
-		System.out.println(localDateTime);
-		System.out.println(dateTime);
-		System.out.println(date);
-	}
+	jr.output();
+  }
 }
+
+class jruby{
+	// TimeZone
+	LocalDateTime localDateTime = LocalDateTime.now();
+	ZonedDateTime dateTime = ZonedDateTime.now(ZoneId.of("Asia/Tokyo"));
+	Date date = new Date();
+
+
+    public void output(){
+      // Output
+	  System.out.println(localDateTime);
+	  System.out.println(dateTime);
+      System.out.println(date);
+    }
+}
+
